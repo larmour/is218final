4d29ecc9adbf3eee1b87ad57ba78c05d984ad2b6
LindseyArmour <la95@njit.edu>
2016-12-20 15:54:55 -0500

    changed dbconn to have singleton pattern

--- includes/config.php
+++ includes/config.php
@@ -1,36 +1,45 @@
 <?php
-//db connection class using singleton pattern
-ob_start();
-session_start();
 
-//set timezone
-date_default_timezone_set('America/New_York');
-
-//database credentials
-define('DBHOST','sql1.njit.edu');
-define('DBUSER','la95');
-define('DBPASS','1uPHeSMbW');
-define('DBNAME','la95');
-
-//application address
-define('DIR','https://web.njit.edu/~la95/is218final/loginregister/');
-define('SITEEMAIL','la95@njit.edu');
-
-try {
-
-	//assign PDO object to db variable
-	$db = new PDO("mysql:host=".DBHOST.";dbname=".DBNAME, DBUSER, DBPASS);
-	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-} catch(PDOException $e) {
-	//output error
-    echo '<p class="bg-danger">'.$e->getMessage().'</p>';
-    exit;
-}
+ class dbConn{
+   protected static $db;
+   private function __construct(){
+   ob_start();
+   session_start();
+   //application address
+   define('DIR','https://web.njit.edu/~la95/is218final/loginregister/');
+   define('SITEEMAIL','la95@njit.edu');
+   
+   //set timezone
+   date_default_timezone_set('America/New_York');
+   
+   try {
+      //create PDO connection
+     self::$db= new PDO('mysql:host=sql1.njit.edu;dbname=la95', 'la95', '1uPHeSMbW' );    
+       self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   } 
+   catch(PDOException $e) {
+      //show error
+   echo '<p class="bg-danger">'.$e->getMessage().'</p>';
+   exit;
+   }
+   }
+   public static function getConnection() {
+     
+     //Guarantees single instance, if no connection object exists then create one.
+     if (!self::$db) {
+     //new connection object.
+     new dbConn();
+     }
+      
+      //return connection.
+      return self::$db;
+      }
+      }
 
 //include the user class, pass in the database connection
 include('classes/user.php');
 include('classes/phpmailer/mail.php');
-$user = new User($db);
+$dbs = dbConn::getConnection();
+$user = new User($dbs);
 ?>
 
